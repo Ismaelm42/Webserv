0c325783396da24e7bf732fe09bd7e3f8bbded8f
Alfonso Fernandez <afz78@hotmail.com>
2024-08-04 20:18:37 +0200

    borrador cgi para comprensión CGI

---
+++ cgifiles/chiquito.php
@@ -0,0 +1,52 @@
+<?php
+
+echo '
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>testCGI</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #000000; /* Fondo negro */
+            color: #FFFFFF; /* Texto blanco */
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .container {
+            max-width: 600px;
+            background-color: #2c3e50; /* Fondo de contenedor oscuro */
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+        }
+        h1 {
+            font-size: 2em;
+            margin-bottom: 10px;
+        }
+	h2 {
+            font-size: 1.8em;
+            margin-bottom: 20px;   
+	}
+        p {
+            font-size: 1.2em;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+	<h1>Webserver</h1>
+	<h2>by Imoro-sa and Alfofern</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+	Pecador pecadorrrrr, no puedor me cago en tus muelas sexuarl. 
+	Hasta luego Lucas torpedo por la gloria de mi madre quietor. 
+	De la pradera ese hombree pecador a grito de nocturnidad no puedor papaar papaar.</p>
+    </div>
+</body>
+</html>';
+?>
+
